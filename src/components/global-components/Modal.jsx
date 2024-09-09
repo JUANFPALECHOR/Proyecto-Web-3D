ee3336cb16c559e0a9e47430996fc4ed4a8604c9
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-09-08 21:54:54 -0500

    modificacion del css del modal

--- src/components/global-components/Modal.jsx
+++ src/components/global-components/Modal.jsx
@@ -10,10 +10,8 @@ const Modal = ({ show, onClose, title, message }) => {
     <div className="modal-overlay" onClick={onClose}> 
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <h1 className='title-modal'>{title}</h1>
-       
         <p dangerouslySetInnerHTML={{ __html: message }}></p>
         <div>
-        <button className="modal-close-button" onClick={onClose}>X</button>
         </div>
         <div>
         <button className="modal-accept-button" onClick={onClose}>Aceptar</button>
